ef37bcfc5299f4c2d2b4ca0f195c594c6d8803bd
ukrWily <ukrwily@gmail.com>
2024-08-08 13:38:13 +0300

    L key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Button from "./components/Button";
+//
+const texts = ["click me", "push me", "hit me", "touch me", "press me"];
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,12 +15,16 @@ function App() {
   return (
     <div className="App">
       <Counter count={count} />
-      <Button onClick={incrementCount} />
-      <Button onClick={incrementCount} />
-      <Button onClick={incrementCount} />
-      <Button onClick={incrementCount} />
+      {texts.map((text, i) => {
+        return <Button text={text} onClick={incrementCount} key={i} />;
+      })}
     </div>
   );
 }
 
 export default App;
+
+// <Button text={texts[0]} onClick={incrementCount} />
+// <Button text={texts[1]} onClick={incrementCount} />
+// <Button text={texts[2]} onClick={incrementCount} />
+// <Button text={texts[3]} onClick={incrementCount} />
